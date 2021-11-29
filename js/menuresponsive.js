d65f58003984376e5fd4e799e77a6a2a2fcde34c
NgSonTung <94957969+NgSonTung@users.noreply.github.com>
2021-11-29 20:05:08 +0700

    add menu dropdown

--- js/menuresponsive.js
+++ js/menuresponsive.js
@@ -1,7 +1,7 @@
 const menubtn = document.getElementById("menu-btn");
 const crossbars = document.getElementById("crossbar-container");
 const menu = document.getElementById("mainmenu");
-const banner = document.getElementById("banner");
+const header = document.getElementById("header");
 
 
 menubtn.addEventListener("click",menuactive);
@@ -12,11 +12,11 @@ crossbars.addEventListener("click",menudeactive);
 function menuactive(){
     crossbars.style.display="block";
     menu.style.height="396px";
-    banner.style.marginTop= "374px";
+    header.style.marginBottom= "374px";
 }
 
 function menudeactive(){
     crossbars.style.display="none"
     menu.style.height="0";
-    banner.style.marginTop= "0";
+    header.style.marginBottom= "0";
 }
